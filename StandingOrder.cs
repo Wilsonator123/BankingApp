d47af63a86a52498349def83bd0c870225e58541
Khadir Abdul <khadir786.ka@googlemail.com>
2024-07-17 23:41:20 +0100

    made class extend transaction class and edit DisplayDetails() method to include transaction ID

--- StandingOrder.cs
+++ StandingOrder.cs
@@ -1,6 +1,6 @@
 ﻿namespace BankingApp
 {
-    public class StandingOrder
+    public class StandingOrder : Transaction
     {
         // could throw exceptions on null arguments
         public string Payee { get; set; }
@@ -11,7 +11,9 @@
         // weekly, monthly, etc.
         public string Interval { get; set; }
 
-        public StandingOrder(string payee, string reference, decimal amount, string interval)
+        public StandingOrder(string payee, string reference, decimal amount, string interval) 
+            : base("Standing Order") 
+  
         {
             // could throw exceptions on null arguments
             Payee = payee;
@@ -25,6 +27,7 @@
         public void DisplayDetails()
         {
             Console.WriteLine($"""
+                              Transaction ID: {TransactionId}
                               Payee: {Payee}
                               Reference: {Reference}
                               Amount: {Amount}
@@ -32,7 +35,22 @@
                               """);
         }
 
+        private DateTime NextPaymentDate()
+        {
+            // TODO: Calculate next payment date
+            // assuming payment first started on transaction's creation date...
+            // Get transaction's creation date
+            // get standing order's interval
+            // get current date
+            // calculate the difference between current date and creation date...
+            //  ... in terms of the interval - call it 'timeDifference'
 
+            // e.g. how many weeks/months has it been since creation date
+            //          can get amount of payments and total paid from this
+            // creationDate + timeDifference + intervalTimeSpan = nextPaymentDate (turn it into a date)
+
+            throw new NotImplementedException();
+        }
 
     }
 }
