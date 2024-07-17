6dbc3d1844325929adcf20a947e5b340c843fcc5
Perry Chen <2558544545qwerty@gmail.com>
2024-07-17 21:02:30 +0000

    Feat: added potential solution for local data storage

---
+++ Globals.cs
@@ -0,0 +1,5 @@
+﻿public static class Globals
+{
+    public static string APP_NAME = "BankingApp";
+    public static string LOCAL_DATA_PATH = $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\{APP_NAME}\";
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ public class Program
         // businessAccount.DisplayAccountInformation();
         // Customer cus = new Customer("George", "Wilson", 100, 100, "24/01/2003");
         // Console.WriteLine(cus.ToString());
-        
-        
+
+
         // // Uncomment this to test the data parser
         // string workingDirectory = Environment.CurrentDirectory;
         // string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
@@ -22,5 +22,19 @@ public class Program
         //     
         // }
 
+        // An example of how to do local data storage
+        //Console.WriteLine(Globals.LOCAL_DATA_PATH);
+        //if (!Directory.Exists(Globals.LOCAL_DATA_PATH))
+        //{
+        //    Directory.CreateDirectory(Globals.LOCAL_DATA_PATH);
+        //}
+        //string businessAccountDataLocation = Globals.LOCAL_DATA_PATH + "BusinessAccount.csv";
+
+        //using (StreamWriter sr = new StreamWriter(businessAccountDataLocation))
+        //{
+        //    sr.WriteLine("AccountName, AccountNumber, AccountBalance, CreationDate, BusinessName, BusinessType, DebitCardNumber, CreditCardNumber, OverdraftAmount, ChequeBookId, LoanRate\r\nMyBusinessAccount1, 12123,    1000m, 17-07-2024, Warner Bros, BusinessType.Partnership, null, 1234512, 1000, #1234, 4.5m\r\nMyBusinessAccount2,12121311, 2421m, 17-01-2024, Barner Bros, BusinessType.Partnership, 2323213, 1234512, 1000, #1231, 2.5m\r\nMyBusinessAccount2,12121311, 1231m, 17-01-2024, Barner Bros, BusinessType.Partnership, 2323213, 1234512, 1000, #1231, 2.5m\r\n");
+        //}
+
+        //Console.WriteLine(businessAccountDataLocation);
     }
 }
