5c05eb010ba2116a758af2493b52da8a35b79ac0
Khadir Abdul <khadir786.ka@googlemail.com>
2024-07-17 21:18:26 +0100

    refactored properties

--- StandingOrder.cs
+++ StandingOrder.cs
@@ -2,11 +2,7 @@
 {
     public class StandingOrder
     {
-        private string _payee = "";
-        private string _reference = "";
-        private decimal _amount = 0.00m;
-        // weekly, monthly, etc.
-        private string _interval = "";
+        
 
         public StandingOrder(string payee, string reference, decimal amount, string interval)
         {
@@ -19,14 +15,14 @@
         }
 
         // could throw exceptions on null arguments
-        public string Payee { get => _payee; set => _payee = value; }
-        public string Reference { get => _reference; set => _reference = value;
-        }
-        public decimal Amount { get => _amount; set => _amount = value; }
+        public string Payee { get; set; }
 
-        public string Interval { get => _interval; set => _interval = value; }
+        public string Reference { get; set; }
 
-        
+        public decimal Amount { get; set; }
+        // weekly, monthly, etc.
+        public string Interval { get; set; }
+
 
         public void DisplayDetails()
         {
