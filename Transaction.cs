63fcedb1ccd06fa2c4cca54ddacb6c3a75ecd4c2
Khadir Abdul <khadir786.ka@googlemail.com>
2024-07-17 20:49:32 +0100

    created initial transaction class

---
+++ Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace BankingApp;
+
+public class Transaction
+{
+    public Transaction(Guid transactionId, string transactionType, DateTime transactionDate)
+    {
+        // could throw exceptions on null arguments
+        TransactionID = transactionId;
+        TransactionType = transactionType;
+        TransactionDate = transactionDate;
+    }
+
+    // could throw exceptions on null arguments
+    public Guid TransactionID { get; set; }
+
+    public string TransactionType { get; set; } = "";
+    public DateTime TransactionDate { get; set; }
+}
+
