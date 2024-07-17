5c56e17186f6b939bba701f1fb47056e3bdb1703
perry-C <2558544545qwerty@gmail.com>
2024-07-17 16:09:30 +0100

    Feat: implemented a csv file parser

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using BankingApp;
-
 public class Program
 {
     public static void Main()
@@ -9,6 +8,19 @@ public class Program
         // businessAccount.DisplayAccountInformation();
         // Customer cus = new Customer("George", "Wilson", 100, 100, "24/01/2003");
         // Console.WriteLine(cus.ToString());
+        
+        
+        // // Uncomment this to test the data parser
+        // string workingDirectory = Environment.CurrentDirectory;
+        // string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        // Console.WriteLine(projectDirectory);
+        // var businessAccountData = DataParser.ReadDataFromCSV(projectDirectory + "/Data/BusinessSampleData.csv");
+        // foreach (var account in businessAccountData)
+        // {
+        //     Console.WriteLine(account["AccountName"]);
+        //     Console.WriteLine(account["AccountBalance"]);
+        //     
+        // }
 
     }
 }
