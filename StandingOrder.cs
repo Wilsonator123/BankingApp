535b72ce6f4e39476a8ce1666aca886bab97b239
Khadir Abdul <khadir786.ka@googlemail.com>
2024-07-19 12:55:09 +0100

    created GetNextPayment method

--- StandingOrder.cs
+++ StandingOrder.cs
@@ -1,4 +1,6 @@
-﻿namespace BankingApp
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace BankingApp
 {
     public class StandingOrder : Transaction
     {
@@ -11,9 +13,11 @@
         // weekly, monthly, etc.
         private string _interval;
 
-        public StandingOrder(string payee, string reference, decimal amount, string interval) 
-            : base("Standing Order") 
-  
+        private DateTime _nextPaymentDate;
+
+        public StandingOrder(string payee, string reference, decimal amount, string interval)
+            : base("Standing Order")
+
         {
             // could throw exceptions on null arguments
             Payee = payee;
@@ -31,6 +35,7 @@
         public decimal Amount { get => _amount; set => _amount = value; }
         // weekly, monthly, etc.
         public string Interval { get => _interval; set => _interval = value; }
+        public DateTime NextPaymentDate { get => _nextPaymentDate; set => _nextPaymentDate = GetNextPaymentDate(); }
 
 
         public void DisplayDetails()
@@ -44,7 +49,7 @@
                               """);
         }
 
-        private DateTime NextPaymentDate()
+        private DateTime GetNextPaymentDate()
         {
             // TODO: Calculate next payment date
             // assuming payment first started on transaction's creation date...
@@ -58,8 +63,61 @@
             //          can get amount of payments and total paid from this
             // creationDate + timeDifference + intervalTimeSpan = nextPaymentDate (turn it into a date)
 
-            throw new NotImplementedException();
+
+            DateTime creationDate = TransactionDate.Date;
+            DateTime currentDate = DateTime.Now.Date;
+            DateTime nextPaymentDate = creationDate;
+            
+            TimeSpan timeDifference = currentDate - creationDate;
+
+            int numberOfIntervals = timeDifference.Days;
+            decimal totalAmountPaid = 0; ;// should split this into a different method
+
+
+            switch (Interval.ToUpper())
+
+            {
+
+                case "DAILY":
+                    nextPaymentDate = creationDate.AddDays(timeDifference.Days + 1);
+                    totalAmountPaid = numberOfIntervals * _amount;
+
+
+                    break;
+                case "WEEKLY":
+                    // can't do add weeks so... convert to weeks + 1... then convert back to days to fit AddDays()
+                    nextPaymentDate = creationDate.AddDays((timeDifference.Days / 7 + 1) * 7);
+                    numberOfIntervals = timeDifference.Days / 7;
+                    totalAmountPaid = numberOfIntervals * _amount;
+
+                    break;
+                case "MONTHLY":
+                    nextPaymentDate = creationDate.AddMonths(1);
+                    numberOfIntervals = timeDifference.Days / 12;
+                    totalAmountPaid = numberOfIntervals * _amount;
+
+                    break;
+                case "QUARTERLY":
+                    nextPaymentDate = creationDate.AddMonths(3);
+                    numberOfIntervals = timeDifference.Days / 4;
+                    totalAmountPaid = numberOfIntervals * _amount;
+
+                    break;
+                case "YEARLY":
+                    nextPaymentDate = creationDate.AddYears(1);
+                    numberOfIntervals = timeDifference.Days / 365;
+                    totalAmountPaid = numberOfIntervals * _amount;
+
+                    break;
+            }
+
+
+
+            return nextPaymentDate;
         }
 
     }
 }
+
+// Should probably make a StandingOrder interface as personal and business accounts can have 
+// standing orders but ISA accounts can't
